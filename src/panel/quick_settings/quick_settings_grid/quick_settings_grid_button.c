5181de86841c105bac8bb18ae2e304ad0fd4f8a4
Reuben Andrews <r.andrews@gns.cri.nz>
2026-09-14 15:57:52 +1200

    fix: handle quick settings buttons without a submenu

--- src/panel/quick_settings/quick_settings_grid/quick_settings_grid_button.c
+++ src/panel/quick_settings/quick_settings_grid/quick_settings_grid_button.c
@@ -168,10 +168,12 @@ void quick_settings_grid_button_set_toggled(QuickSettingsGridButton *self,
                                             gboolean toggled) {
     if (toggled) {
         gtk_widget_remove_css_class(GTK_WIDGET(self->toggle), "off");
-        gtk_widget_remove_css_class(GTK_WIDGET(self->reveal_button), "off");
+        if (self->reveal_button)
+            gtk_widget_remove_css_class(GTK_WIDGET(self->reveal_button), "off");
     } else {
         gtk_widget_add_css_class(GTK_WIDGET(self->toggle), "off");
-        gtk_widget_add_css_class(GTK_WIDGET(self->reveal_button), "off");
+        if (self->reveal_button)
+            gtk_widget_add_css_class(GTK_WIDGET(self->reveal_button), "off");
     }
 }
 
