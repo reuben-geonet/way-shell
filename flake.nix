3e98b6000c9a4fc2d00ed049f8712be2d34e93bd
Reuben Andrews <r.andrews@gns.cri.nz>
2026-09-17 07:48:18 +1200

    nix: update flake to follow branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Way-Shell development, native packages and verified Fedora RPMs";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/eaad089433ca2bb662274377d33df3d0e51ef28b";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
