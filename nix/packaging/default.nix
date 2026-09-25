b11efb2e0c657d36d94a691b70576d8d95ebbf4b
Reuben Andrews <r.andrews@gns.cri.nz>
2026-09-25 14:16:35 +1200

    nix: assemble release bundle

--- nix/packaging/default.nix
+++ nix/packaging/default.nix
@@ -10,14 +10,24 @@
         text = ''exec python3 ${../../scripts/bump-version.py} "$@"'';
       });
     };
-    packages.packages = pkgs.runCommand "way-shell-packages" { } ''
-      mkdir -p "$out"
-      mkdir -p "$out/rpms"
-      ${lib.concatMapStringsSep "\n" (release: ''
-        cp ${config.packages."package-fedora-${release}"}/release/*.rpm "$out/rpms/"
-      '') (builtins.attrNames config.wayShell.fedora.releases)}
-      mkdir -p "$out/arch"
-      cp ${config.packages.package-arch}/release/*.pkg.tar.zst "$out/arch/"
-    '';
+    packages = {
+      release-source-files = pkgs.runCommand "way-shell-release-source-files" { } ''
+        mkdir -p "$out/release"
+        cp ${config.packages.release-archive} "$out/release/way-shell-${config.wayShell.version}.tar.gz"
+        cp ${config.packages.release-pkgbuild} "$out/release/PKGBUILD"
+      '';
+      packages = pkgs.runCommand "way-shell-packages" { } ''
+        mkdir -p "$out/release" "$out/logs"
+        cp ${config.packages.release-source-files}/release/* "$out/release/"
+        ${lib.concatMapStringsSep "\n" (release: ''
+          cp ${config.packages."package-fedora-${release}"}/release/*.rpm "$out/release/"
+          cp -r ${config.packages."package-fedora-${release}"}/logs "$out/logs/fedora-${release}"
+        '') (builtins.attrNames config.wayShell.fedora.releases)}
+        cp ${config.packages.package-arch}/release/*.pkg.tar.zst "$out/release/"
+        cp -r ${config.packages.package-arch}/logs "$out/logs/arch"
+        cd "$out/release"
+        sha256sum -- * > SHA256SUMS
+      '';
+    };
   };
 }
